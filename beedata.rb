502225f07c78efc09b4f8793a43acee221fda2b3
Kingdon Barrett <yebyen@gmail.com>
2014-12-11 09:26:09 -0500

    after tomorrow, data turns around again
    will eventually rewrite this as "after {datapoint}, turn around" so I
    can write simply like "turn around once you get to 0", "count up to 275
    and go back", etc

--- beedata.rb
+++ beedata.rb
@@ -87,7 +87,11 @@ def beedata(arr)
       unless now_date > Date.parse('2014-05-01')
         new_value = last_value+1
       else
-        new_value = last_value-1
+        unless now_date > Date.parse('2014-12-12')
+          new_value = last_value-1
+        else
+          new_value = last_value+1
+        end
       end
     end
     puts "sending: #{new_value}"
